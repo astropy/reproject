5274065bea34ca9e713658de10b11fad70a0197e
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-05-12 11:52:25 +0100

    Added Pythonic wrapper to computeOverlap

---
+++ reproject/overlap.py
@@ -0,0 +1,25 @@
+from ._overlap_wrapper import _computeOverlap
+
+def compute_overlap(ilon, ilat, olon, olat, energy_mode=False, reference_area=1.):
+    """
+    Compute the overlap between two 'pixels' in spherical coordinates
+    
+    Parameters
+    ----------
+    ilon : np.ndarray
+        The longitudes defining the four corners of the input pixel
+    ilat : np.ndarray
+        The latitudes defining the four corners of the input pixel
+    olon : np.ndarray
+        The longitudes defining the four corners of the output pixel
+    olat : np.ndarray
+        The latitudes defining the four corners of the output pixel
+    energy_mode : bool
+        Whether to work in energy-conserving or surface-brightness-conserving mode
+    reference_area : float
+        To be determined
+    """
+    area_ratio = 1.
+    return _computeOverlap(ilon, ilat, olon, olat, int(energy_mode), reference_area, area_ratio)
+    
+    
