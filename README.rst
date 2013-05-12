fb535765e4f84f26c07dd5be10d88579860cb581
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-05-12 11:34:48 +0100

    Initial code wrapping computeOverlap

---
+++ README.rst
@@ -0,0 +1,8 @@
+About
+=====
+
+Python wrapper to the core Montage spherical polygon intersection. The overlap
+code was lifted from the Montage package with permission from the authors. For
+more information about Montage:
+
+    http://http://montage.ipac.caltech.edu
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+
+from distutils.core import setup, Extension
+from distutils.command.sdist import sdist
+
+try:  # Python 3.x
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:  # Python 2.x
+    from distutils.command.build_py import build_py
+
+from numpy import get_include as get_numpy_include
+
+from hyperion.testing.helper import HyperionTest
+from hyperion.version import __version__, __dev__
+
+numpy_includes = get_numpy_include()
+
+ext_modules = [Extension("reproject._overlap_wrapper",
+                         ['reproject/_overlap_wrapper.c', 'reproject/overlapArea.c'],
+                         include_dirs=[numpy_includes])]
+
+setup(name='reproject',
+      version=__version__,
+      author='Thomas Robitaille',
+      author_email='thomas.robitaille@gmail.com',
+      packages=['reproject'],
+      ext_modules = ext_modules
+     )
