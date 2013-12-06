b7b2f3d135c78dc7be1ff1b4068343079507126d
Christoph Deil <Deil.Christoph@gmail.com>
2013-12-06 10:15:36 +0100

    Improve compute_overlap docstring

--- reproject/overlap.py
+++ reproject/overlap.py
@@ -4,18 +4,24 @@ __all__ = ['compute_overlap']
 
 
 def compute_overlap(ilon, ilat, olon, olat):
-    """
-    Compute the overlap between two 'pixels' in spherical coordinates
+    """Compute the overlap between two 'pixels' in spherical coordinates.
     
     Parameters
     ----------
     ilon : np.ndarray
-        The longitudes defining the four corners of the input pixel
+        The longitudes (in deg) defining the four corners of the input pixel
     ilat : np.ndarray
-        The latitudes defining the four corners of the input pixel
+        The latitudes (in deg) defining the four corners of the input pixel
     olon : np.ndarray
-        The longitudes defining the four corners of the output pixel
+        The longitudes (in deg) defining the four corners of the output pixel
     olat : np.ndarray
-        The latitudes defining the four corners of the output pixel
+        The latitudes (in deg) defining the four corners of the output pixel
+    
+    Returns
+    -------
+    overlap : np.ndarray
+        Pixel overlap solid angle in steradians
+    area_ratio : np.ndarray
+        TODO
     """
     return _computeOverlap(ilon, ilat, olon, olat, 0, 1.)
