a01d78e258107f07891d26521c493fd4887f2bbb
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-12-06 09:20:30 +0100

    Added infrastructure for testing

--- setup.py
+++ setup.py
@@ -2,13 +2,10 @@
 
 import os
 
-from distutils.core import setup, Extension
+from distutils.core import setup, Extension, Command
 from distutils.command.sdist import sdist
 
-try:  # Python 3.x
-    from distutils.command.build_py import build_py_2to3 as build_py
-except ImportError:  # Python 2.x
-    from distutils.command.build_py import build_py
+from distutils.command.build_py import build_py
 
 from numpy import get_include as get_numpy_include
 
@@ -18,10 +15,23 @@ ext_modules = [Extension("reproject._overlap_wrapper",
                          ['reproject/_overlap_wrapper.c', 'reproject/overlapArea.c'],
                          include_dirs=[numpy_includes])]
 
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import sys,subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+
 setup(name='reproject',
       version="0.1.0",
       author='Thomas Robitaille',
       author_email='thomas.robitaille@gmail.com',
       packages=['reproject'],
+      cmdclass = {'test': PyTest},
       ext_modules = ext_modules
      )
