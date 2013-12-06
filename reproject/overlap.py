1b2f9b53f9b60e7e26ee7b6edcc479a33a630672
Christoph Deil <Deil.Christoph@gmail.com>
2013-12-06 04:13:08 +0100

    Add solid_angle function.
    And document input / output units for compute_overlap and solid_angle
    function.

--- reproject/overlap.py
+++ reproject/overlap.py
@@ -1,22 +1,52 @@
 from ._overlap_wrapper import _computeOverlap
 
-__all__ = ['compute_overlap']
+__all__ = ['compute_overlap', 'solid_angle']
+
 
 def compute_overlap(ilon, ilat, olon, olat):
-    """
-    Compute the overlap between two 'pixels' in spherical coordinates
+    """Compute the overlap between two 'pixels' in spherical coordinates.
     
     Parameters
     ----------
     ilon : np.ndarray
-        The longitudes defining the four corners of the input pixel
+        The longitudes (in deg) defining the four corners of the input pixel
     ilat : np.ndarray
-        The latitudes defining the four corners of the input pixel
+        The latitudes (in deg) defining the four corners of the input pixel
     olon : np.ndarray
-        The longitudes defining the four corners of the output pixel
+        The longitudes (in deg) defining the four corners of the output pixel
     olat : np.ndarray
-        The latitudes defining the four corners of the output pixel
+        The latitudes (in deg) defining the four corners of the output pixel
+    
+    Returns
+    -------
+    overlap : np.ndarray
+        Pixel overlap solid angle in steradians
+    area_ratio : np.ndarray
+        TODO
     """
     return _computeOverlap(ilon, ilat, olon, olat, 0, 1.)
-    
-    
+
+
+def solid_angle(lon, lat):
+    """Compute the area of 'pixels' in spherical coordinates.     
+
+    Parameters
+    ----------
+    lon : np.ndarray
+        The longitudes defining the four corners of the pixel
+    lat : np.ndarray
+        The latitudes defining the four corners of the pixel
+
+    Returns
+    -------
+    solid_angle : np.ndarray
+        Pixel solid angle in steradians
+    """
+    return _computeOverlap(lon, lat, lon, lat, 0, 1.)[0]
+
+
+def test_solid_angle():
+    import numpy as np
+    lon, lat = [0, 1, 1, 0], [0, 0, 1, 1]
+    omega = solid_angle(lon, lat)
+    np.testing.assert_allclose(omega, np.radians(1) ** 2, rtol=1e-3)
