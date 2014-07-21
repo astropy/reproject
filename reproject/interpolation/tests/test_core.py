55cf04ea05887528901f531cdf1c47ce93f46ea1
Christoph Deil <Deil.Christoph@gmail.com>
2014-07-21 11:38:22 +0200

    Fix an import in tests

--- reproject/interpolation/tests/test_core.py
+++ reproject/interpolation/tests/test_core.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-from ..wcs_utils import wcs_to_celestial_frame
+from ...wcs_utils import wcs_to_celestial_frame
 from .. import interpolate_2d, interpolate_celestial_slices
 
 
