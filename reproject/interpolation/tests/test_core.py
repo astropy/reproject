237aa18fb5dafc715f0553af9553783b67f3f2ee
Christoph Deil <Deil.Christoph@gmail.com>
2014-07-21 10:57:37 +0200

    Add test files for interpolation

---
+++ reproject/interpolation/tests/test_core.py
@@ -0,0 +1,13 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import print_function, division
+
+from .. import interpolate_2d, interpolate_celestial_slices
+
+
+def test_interpolate_2d():
+    assert True
+
+
+def test_interpolate_celestial_slices():
+    assert True
+
