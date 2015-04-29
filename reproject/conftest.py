2be5e45b57327421aaa33ebec7f39d88c034dccf
Brigitta Sipocz <bsipocz@gmail.com>
2015-04-29 16:09:31 +0200

    Adding test header customisation

--- reproject/conftest.py
+++ reproject/conftest.py
@@ -2,8 +2,30 @@
 # by importing them here in conftest.py they are discoverable by py.test
 # no matter how it is invoked within the source tree.
 
+import os
 from astropy.tests.pytest_plugins import *
 
-## Uncomment the following line to treat all DeprecationWarnings as
-## exceptions
+from . import version
+
+
+# Uncomment the following line to treat all DeprecationWarnings as
+# exceptions
 enable_deprecations_as_exceptions()
+
+# Add astropy to test header information and remove unused packages.
+# Pytest header customisation was introduced in astropy 1.0.
+
+try:
+    PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
+    del PYTEST_HEADER_MODULES['h5py']
+    del PYTEST_HEADER_MODULES['Matplotlib']
+except NameError:  # needed to support Astropy < 1.0
+    pass
+
+
+# This is to figure out reproject version, rather than using Astropy's
+try:
+    packagename = os.path.basename(os.path.dirname(__file__))
+    TESTED_VERSIONS[packagename] = version.version
+except NameError:   # Needed to support Astropy <= 1.0.0
+    pass
