789a651b5d5fb4e29893078114cade82414be2de
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-05-31 15:13:45 +0200

    Updated infrastructure files to package-template 1.0

--- reproject/conftest.py
+++ reproject/conftest.py
@@ -5,16 +5,13 @@
 import os
 from astropy.tests.pytest_plugins import *
 
-from . import version
-
-
 # Uncomment the following line to treat all DeprecationWarnings as
 # exceptions
 enable_deprecations_as_exceptions()
 
-# Add astropy to test header information and remove unused packages.
-# Pytest header customisation was introduced in astropy 1.0.
-
+# Uncomment and customize the following lines to add/remove entries
+# from the list of packages for which version numbers are displayed
+# when running the tests
 try:
     PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
     del PYTEST_HEADER_MODULES['h5py']
@@ -22,8 +19,15 @@ try:
 except NameError:  # needed to support Astropy < 1.0
     pass
 
+# Uncomment the following lines to display the version number of the
+# package rather than the version number of Astropy in the top line when
+# running the tests.
+import os
 
-# This is to figure out reproject version, rather than using Astropy's
+# This is to figure out the affiliated package version, rather than
+# using Astropy's
+from . import version
+
 try:
     packagename = os.path.basename(os.path.dirname(__file__))
     TESTED_VERSIONS[packagename] = version.version
