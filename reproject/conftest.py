d3cddeea2c5aae26ea56481b2e804d0c120da3c2
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-06-01 12:05:45 +0100

    Updated to package-template v1.1

--- reproject/conftest.py
+++ reproject/conftest.py
@@ -2,22 +2,22 @@
 # by importing them here in conftest.py they are discoverable by py.test
 # no matter how it is invoked within the source tree.
 
-import os
 from astropy.tests.pytest_plugins import *
 
 # Uncomment the following line to treat all DeprecationWarnings as
 # exceptions
 enable_deprecations_as_exceptions()
 
-# Uncomment and customize the following lines to add/remove entries
-# from the list of packages for which version numbers are displayed
-# when running the tests
+# Uncomment and customize the following lines to add/remove entries from
+# the list of packages for which version numbers are displayed when running
+# the tests. Making it pass for KeyError is essential in some cases when
+# the package uses other astropy affiliated packages.
 try:
     PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
     PYTEST_HEADER_MODULES['Healpy'] = 'healpy'
     del PYTEST_HEADER_MODULES['h5py']
     del PYTEST_HEADER_MODULES['Matplotlib']
-except NameError:  # needed to support Astropy < 1.0
+except (NameError, KeyError):  # NameError is needed to support Astropy < 1.0
     pass
 
 # Uncomment the following lines to display the version number of the
@@ -27,10 +27,13 @@ import os
 
 # This is to figure out the affiliated package version, rather than
 # using Astropy's
-from . import version
+try:
+    from .version import version
+except ImportError:
+    version = 'dev'
 
 try:
     packagename = os.path.basename(os.path.dirname(__file__))
-    TESTED_VERSIONS[packagename] = version.version
+    TESTED_VERSIONS[packagename] = version
 except NameError:   # Needed to support Astropy <= 1.0.0
     pass
