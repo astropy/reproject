85585da0c66cfd535a4719fa1288cd7eef49ce01
Sam Van Kooten <vankooten.sam@gmail.com>
2022-08-23 11:09:52 -0600

    Remove deprecated 'order' arg from reproject_adaptive

--- reproject/adaptive/high_level.py
+++ reproject/adaptive/high_level.py
@@ -1,16 +1,12 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-import astropy.utils
-
 from ..utils import parse_input_data, parse_output_projection
 from .core import _reproject_adaptive_2d
 
 __all__ = ['reproject_adaptive']
 
 
-@astropy.utils.deprecated_renamed_argument('order', None, since=0.9)
 def reproject_adaptive(input_data, output_projection, shape_out=None, hdu_in=0,
-                       order=None,
                        return_footprint=True, center_jacobian=False,
                        roundtrip_coords=True, conserve_flux=False,
                        kernel='gaussian', kernel_width=1.3,
@@ -50,9 +46,6 @@ def reproject_adaptive(input_data, output_projection, shape_out=None, hdu_in=0,
     hdu_in : int or str, optional
         If ``input_data`` is a FITS file or an `~astropy.io.fits.HDUList`
         instance, specifies the HDU to use.
-    order : str
-        Deprecated, and no longer has any effect. Will be removed in a future
-        release.
     return_footprint : bool
         Whether to return the footprint in addition to the output array.
     center_jacobian : bool
